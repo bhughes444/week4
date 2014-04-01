9076aaaf296565cf062fb0027befc83506baa221
Brandon Hughes <bhughes444@gmail.com>
2014-04-01 11:48:53 -0500

    Modifiedper lesson plan

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,17 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope', function($scope) {
 
+	$scope.car = {make:'Honda', model:'Civic', year:2014, price:24000};
+  
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope', function($scope) {
 
+	$scope.cars = [
+		{make:'Ford', model:'Mustang', year:2014, price:65000},
+		{make:'Chevy', model:'Cruz', year:2011, price:10000},
+		{make:'Nissan', model:'Maxima', year:2012, price:20000},
+		{make:'Toyota', model:'Supra', year:2010, price:45000}];
+  
   }]);
